85813f5f77ecfde451a988f581e5ea16b56bcea4
Noah Heck <myesain84@gmail.com>
2018-05-18 22:36:04 -0600

    Re-alias the DebugBar facade

--- config/app.php
+++ config/app.php
@@ -212,7 +212,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'DebugBar' => Barryvdh\Debugbar\Facade::class,
         'Image' => Intervention\Image\Facades\Image::class,
 
     ],
