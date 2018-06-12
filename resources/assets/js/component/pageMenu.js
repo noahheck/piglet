33438d933ede3e0e7cb97ada9011ddfbb895869f
Noah Heck <myesain84@gmail.com>
2018-06-11 23:16:35 -0600

    Hide menu and add animations for showing

--- resources/assets/js/component/pageMenu.js
+++ resources/assets/js/component/pageMenu.js
@@ -4,4 +4,10 @@
 
 let $ = require('jquery');
 
+$(function() {
+    $("#pageMenuDropdownTrigger").click(function() {
+        $(this).toggleClass("fa-chevron-down fa-chevron-up");
 
+        $("#pageMenuDropdownContent").slideToggle(150);
+    })
+});
