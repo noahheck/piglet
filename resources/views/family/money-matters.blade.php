4161dee429d80c7b850ecd8954748105a9794043
Noah Heck <myesain84@gmail.com>
2018-06-17 16:19:03 -0600

    Add 2 options for money-matters nav

--- resources/views/family/money-matters.blade.php
+++ resources/views/family/money-matters.blade.php
@@ -17,9 +17,42 @@
     @include('family.shared.breadcrumb', [
         'breadcrumb' => [],
         'location'   => 'Money Matters',
-        /*'menu' => [
-            ['type' => 'link', 'href' => route('family.members.create', [$family]), 'icon' => 'fa fa-plus-circle', 'text' => 'Add New'],
-        ]*/
+        'menu' => [
+            ['type' => 'link', 'href' => '#', 'icon' => 'fa fa-bar-chart', 'text' => 'Reports'],
+            ['type' => 'link', 'href' => '#', 'icon' => 'fa fa-building', 'text' => 'Merchants'],
+            ['type' => 'link', 'href' => '#', 'icon' => 'fa fa-cogs', 'text' => 'Settings'],
+        ]
     ])
+            {{--['type' => 'link', 'href' => route('family.members.create', [$family]), 'icon' => 'fa fa-plus-circle', 'text' => 'Add New'],--}}
+
+    {{--<div class="row">
+
+        <div class="col">
+            <a class="card shadow" href="#">
+                <div class="card-body">
+                    <span class="fa fa-bar-chart"></span> Reports
+                </div>
+            </a>
+        </div>
+
+        <div class="col">
+            <a class="card shadow" href="#">
+                <div class="card-body">
+                    <span class="fa fa-building"></span> Merchants
+                </div>
+            </a>
+        </div>
+
+        <div class="col">
+            <a class="card shadow" href="#">
+                <div class="card-body">
+                    <span class="fa fa-cogs"></span> Settings
+                </div>
+            </a>
+        </div>
+
+    </div>
+
+    <hr>--}}
 
 @endsection
