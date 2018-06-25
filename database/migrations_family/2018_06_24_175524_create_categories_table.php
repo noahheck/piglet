e664307e6fb1e54ccff43c9851fc9deb999c9f2b
Noah Heck <myesain84@gmail.com>
2018-06-24 19:30:02 -0600

    Add default categories

--- database/migrations_family/2018_06_24_175524_create_categories_table.php
+++ database/migrations_family/2018_06_24_175524_create_categories_table.php
@@ -22,6 +22,57 @@ class CreateCategoriesTable extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        \Illuminate\Support\Facades\DB::connection('family')->table('categories')->insert(
+            [[
+                'name'        => 'Food',
+                'active'      => 1,
+                'description' => 'Restaurants, Groceries',
+                'd_order'     => '1',
+            ],
+            [
+                'name'        => 'Housing',
+                'active'      => 1,
+                'description' => 'Rent/Mortgage, HOA Dues, Repairs/Maintenance',
+                'd_order'     => '2',
+            ],
+            [
+                'name'        => 'Utilities',
+                'active'      => 1,
+                'description' => 'Electricity, Gas, Water, Phone, Internet',
+                'd_order'     => '3',
+            ],
+            [
+                'name'        => 'Transportation',
+                'active'      => 1,
+                'description' => 'Car Payments, Fuel, Oil Changes, Repairs, Licenses, Taxes',
+                'd_order'     => '4',
+            ],
+            [
+                'name'        => 'Personal',
+                'active'      => 1,
+                'description' => 'Toiletries, Cosmetics, Household Supplies, Pet Supplies',
+                'd_order'     => '5',
+            ],
+            [
+                'name'        => 'Health Care',
+                'active'      => 1,
+                'description' => 'Medical/Dental/Vision, Prescriptions',
+                'd_order'     => '6',
+            ],
+            [
+                'name'        => 'Insurance',
+                'active'      => 1,
+                'description' => 'Medical, Dental, Vision, Life, Homeowner, Automobile Premiums',
+                'd_order'     => '7',
+            ],
+            [
+                'name'        => 'Recreation',
+                'active'      => 1,
+                'description' => 'Vacation, Sports, Entertainment, Subscriptions, Hobbies',
+                'd_order'     => '8',
+            ]]
+        );
     }
 
     /**
