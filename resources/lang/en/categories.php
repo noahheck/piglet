2df02ec5040d13d56d394e7f3dbf19bd1b719243
Noah Heck <myesain84@gmail.com>
2018-06-27 19:30:13 -0600

    Translations for default categories in migrations

--- resources/lang/en/categories.php
+++ resources/lang/en/categories.php
@@ -21,4 +21,37 @@ return [
     'description'  => 'Description',
 
 
+    // Default categories added with the migration:
+    'default.food'                => 'Food',
+    'default.food.desc'           => 'Food expenses',
+    'default.food.subs'           => '["Groceries","Restaurants","Coffee Shops"]',
+
+    'default.housing'             => 'Housing',
+    'default.housing.desc'        => "Expenses related to owning or renting your home and it's upkeep",
+    'default.housing.subs'        => '["Mortgage","Rent","Repairs","Maintenance","Taxes","HOA Dues"]',
+
+    'default.utilities'           => 'Utilities',
+    'default.utilities.desc'      => 'Expenses related to operating the equipment in your home',
+    'default.utilities.subs'      => '["Electricity","Gas","Water","Cable","Internet"]',
+
+    'default.transportation'      => 'Transportation',
+    'default.transportation.desc' => 'Expenses related to maintaining a personal vehicle or other means of personal transportation',
+    'default.transportation.subs' => '["Car Payment","Bus Pass","Taxi","Fuel","Oil Changes","Repairs","Licenses","Taxes"]',
+
+    'default.personal'            => 'Personal',
+    'default.personal.desc'       => 'Household/Living expenses',
+    'default.personal.subs'       => '["Toiletries","Cosmetics","Household Supplies","Pet Supplies","Furniture"]',
+
+    'default.healthcare'          => 'Health Care',
+    'default.healthcare.desc'     => 'Expenses for your professional health care services',
+    'default.healthcare.subs'     => '["Medical","Dental","Vision","Prescriptions","Orthodontic"]',
+
+    'default.insurance'           => 'Insurance',
+    'default.insurance.desc'      => 'Premiums paid for maintaining insurance policies',
+    'default.insurance.subs'      => '["Medical","Dental","Vision","Life","Homeowner","Automobile"]',
+
+    'default.recreation'          => 'Recreation',
+    'default.recreation.desc'     => 'All the things that make life worth living',
+    'default.recreation.subs'     => '["Vacation","Sports","Entertainment","Subscriptions","Hobbies"]',
+
 ];
