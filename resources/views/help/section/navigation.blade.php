c6cb7f671a7d0b13eb057c5504a158f51579717c
Noah Heck <noah@noahheck.com>
2018-12-16 09:07:59 -0700

    Move files

--- resources/views/help/section/navigation.blade.php
+++ resources/views/help/section/navigation.blade.php
@@ -1,7 +1,7 @@
 <h1>{{ __('help/navigation.navigation') }}</h1>
 
 <div class="image-container">
-    <img src="{{ asset('img/welcome/top_nav.png') }}" alt="Main navigation bar across the top of the screen">
+    <img src="{{ asset('img/help/navigation/top_nav.png') }}" alt="Main navigation bar across the top of the screen">
 </div>
 
 {!! __('help/navigation.main-navigation-details') !!}
@@ -9,11 +9,11 @@
 <hr>
 
 <div class="image-container">
-    <img src="{{ asset('/img/welcome/family_nav.png') }}" alt="Family navigation bar showing dropdown menu in closed state">
+    <img src="{{ asset('/img/help/navigation/family_nav.png') }}" alt="Family navigation bar showing dropdown menu in closed state">
 </div>
 
 {!! __('help/navigation.family-navigation-details') !!}
 
 <div class="image-container">
-    <img src="{{ asset('/img/welcome/page_menu.png') }}" alt="Family navigation bar showing dropdown menu in open state">
+    <img src="{{ asset('/img/help/navigation/page_menu.png') }}" alt="Family navigation bar showing dropdown menu in open state">
 </div>
