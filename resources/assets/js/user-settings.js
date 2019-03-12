14cba9609885739c72c257007a12b3d9ca63a8c3
Noah Heck <noah@noahheck.com>
2019-03-11 18:41:14 -0600

    Provide live preview when changing bkgrd color

--- resources/assets/js/user-settings.js
+++ resources/assets/js/user-settings.js
@@ -7,8 +7,14 @@ let meta = require('Services/meta');
 
 $(function() {
 
+    let colorField = $('#background_color');
+
     $('#restoreBackgroundColorButton').click(() => {
-        $('#background_color').val(meta.get('default-background-color'));
+        colorField.val(meta.get('default-background-color')).change();
+    });
+
+    colorField.change(() => {
+        $('body').css('backgroundColor', colorField.val());
     });
 
 });
