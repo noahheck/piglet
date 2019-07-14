d65f908682b0e59927194f2a859d91102f0e1b73
Noah Heck <noah@noahheck.com>
2019-07-14 11:32:50 -0600

    Close modal on back button if modal open
    Also, scroll to top when loading another help section in the open modal

--- resources/assets/js/component/modal.js
+++ resources/assets/js/component/modal.js
@@ -8,8 +8,27 @@ let modal = {};
 
 let appModal;
 
+function hashTitle(title) {
+    return title.replace(/\s+/gi, '-').toLowerCase();
+}
+
 $(function(){
     appModal = $('#applicationModal').modal({show: false});
+
+
+    appModal.on('hide.bs.modal', function() {
+        if (window.location.hash === currentHash) {
+            window.history.back();
+
+            currentHash = '';
+        }
+    });
+
+    $(window).on('popstate', function () {
+
+        appModal.modal('hide');
+    });
+
 });
 
 
@@ -25,15 +44,29 @@ modal.setTitle = function(title) {
 modal.setContent = function(content) {
     let modalBody = appModal.find('#modalBody');
     modalBody.empty().html(content);
+
+    if (appModal.data('bs.modal')._isShown) {
+        appModal.animate({ scrollTop: 0 }, 'fast');
+    }
 };
 
+let currentHash = '';
 
 modal.display = function(title, content) {
 
     modal.setTitle(title);
     modal.setContent(content);
 
+    let hashReadyTitle = hashTitle(title);
+    currentHash = '#' +  hashReadyTitle;
+
+    if (currentHash !== window.location.hash) {
+        window.history.pushState('forward', null, '#' + hashReadyTitle);
+    }
+
     appModal.modal('show');
 };
+
+
 
 module.exports = modal;
